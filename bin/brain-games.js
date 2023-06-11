e6dd18805714b5e423be917e4197ac0db832ac63
salkyn <omursalkyn@gmail.com>
2023-06-12 00:30:05 +0600

    add 3 step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-Welcome to the Brain Games!
+//<p>Welcome to the Brain Games!</p>
+import userName from '../src/cli.js'
+console.log('Hello,', userName+'!');
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync';
+const name = readlineSync.question('May I have your name? ');
+export default name;
