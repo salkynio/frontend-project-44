90c801ed192de97cacc7c4dd46e5b885e4c7f606
salkyn <omursalkyn@gmail.com>
2023-06-13 09:59:40 +0600

    add changes to brain.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-//<p>Welcome to the Brain Games!</p>
-import userName from '../src/cli.js'
-console.log('Hello,', userName+'!');
+
+import greeting from '../src/cli.js';
+
+greeting();
