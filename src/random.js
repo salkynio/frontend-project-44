b4954187bf52f60303dea5210f0699e8a7e44232
salkyn <omursalkyn@gmail.com>
2023-06-16 12:48:37 +0600

    add changes to random.js

--- src/random.js
+++ src/random.js
@@ -1,5 +1,8 @@
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+const getRandom = (max) => Math.round(Math.random() * max);
 
+// function getRandomRange(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// export default getRandomRange;
 export default getRandom;
